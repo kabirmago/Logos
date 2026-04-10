eb16f4a538e276b4d1cf4975994aae93eb1d7c19
Kabir Mago <kabirmago@gmail.com>
2026-04-10 11:40:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({mode}) => {
     },
     server: {
   hmr: process.env.DISABLE_HMR !== 'true',
-  allowedHosts: ['logos-production-fe34.up.railway.app'],
+  allowedHosts: ['logos-production-1d63.up.railway.app'],
 },
   };
 });
