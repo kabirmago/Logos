d18c0a5514e6bd8ae7f9a2845ef98535d6ef42ae
Kabir Mago <kabirmago@gmail.com>
2026-04-16 22:33:19 -0400

    Update allowedHosts in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({mode}) => {
     },
     server: {
   hmr: process.env.DISABLE_HMR !== 'true',
-  allowedHosts: ['logos-production-1d63.up.railway.app'],
+  allowedHosts: ['logosapp.me'],
 },
   };
 });
